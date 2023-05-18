79f8b6b1132cb1d3eb33ef1e971b66e975e577b3
Shaun Lindsley <shaunlindsley@gmail.com>
2023-05-18 18:21:12 +0100

    Update paprkia url condition

--- src/api/base.js
+++ src/api/base.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
-axios.defaults.baseURL = import.meta.env.VITE_PAPRIKA_URL;
+axios.defaults.baseURL = import.meta.env.VITE_PAPRIKA_URL
+  ? import.meta.env.VITE_PAPRIKA_URL
+  : "https://api.coinpaprika.com/v1/";
 
 export const baseApi = axios.create({});
